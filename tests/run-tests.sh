e9eb7ab3b7e5b13c2f9af11e958d7012a9aa29c5
David Hempston <cyber_veio@hotmail.com>
2023-08-11 16:20:09 +0100

    Wait for jobs to finish for 100 secs

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,5 +2,19 @@
 
 sudo /opt/pbs/bin/pbsnodes -a
 /opt/pbs/bin/qstat -q
-/opt/pbs/bin/qsub tests/simple-job.pbs
-/opt/pbs/bin/qstat -f
+/opt/pbs/bin/qsub tests/*.pbs
+
+QUEUE="workq"
+RUNNING=$(qstat -q | grep "$QUEUE" | awk '{ print $6 }')
+QUEUED=$(qstat -q | grep "$QUEUE" | awk '{ print $7 }')
+n=0
+if [[ $RUNNING -eq 0 && $QUEUED -eq 0 ]]; then
+    exit 0
+else
+    echo "Still waiting"
+    n=$(( n + 1))
+    if [[ $n -ge 10 ]]; then
+        exit 1
+    fi
+    sleep 10
+fi
