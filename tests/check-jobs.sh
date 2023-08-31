6fcd7f70f24073c400aefa8966d9a7252a6e3c9b
David Hempston <cyber_veio@hotmail.com>
2023-08-31 15:17:55 +0100

    Checking final job queue

---
+++ tests/check-jobs.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+EXIT_CODE=0
+for line in $(qstat | grep runner); do 
+  job_target=$(echo "line" | awk '{ print $2}' | cut -d"-" -f1)
+  final_queue=$(echo "line" | awk '{ $6 }'
+  if [[ "${job_target}" != "${final_qeueu}" ]]; then
+    echo "Error found in: ${line}"
+    EXIT_CODE=1
+  fi
+exit ${EXIT_CODE}
+
