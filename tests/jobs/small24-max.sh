ec71c4043d187cb030d52e4f8a29a4fdd4463387
David Hempston <cyber_veio@hotmail.com>
2023-08-31 13:56:54 +0100

    Small test jobs

---
+++ tests/jobs/small24-max.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=128:mem=1gb
+#PBS -lselect=24:0:0
+#PBS -N v1_small24
+#PBS -o /dev/null
+#PBS -e /dev/null
+
---
+++ tests/jobs/small24-min.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=1:mem=1gb
+#PBS -lselect=0:0:1
+#PBS -N v1_small24
+#PBS -o /dev/null
+#PBS -e /dev/null
+
