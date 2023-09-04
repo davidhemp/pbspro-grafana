c48e0317ec8117dd22c35389930e3337c2f73c75
David Hempston <cyber_veio@hotmail.com>
2023-09-04 16:38:02 +0100

    Using mv to install influxdb and fixed typo

--- tests/influxdb/install.sh
+++ tests/influxdb/install.sh
@@ -3,12 +3,12 @@ export INFLUXDB_VERSION="2.7.1"
 export INFLUXDB_DOWNLOAD_PATH="https://dl.influxdata.com/influxdb/releases"
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
 tar xvfz influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
-sudo cp influxdb2_linux_amd64/influxd /usr/local/bin/
+sudo mv influxdb2_linux_amd64/influxd /usr/local/bin/
 
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-client-$INFLUXDB_VERSION-linux-amd64.tar.gz
 tar xvfz influxdb2-client-$INFLUXDB_VERSION-linux-amd64.tar.gz
-sudo cp influx /usr/local/bin/
-export PATH:/usr/local/bin/:$PATH
+sudo mv influx /usr/local/bin/
+export PATH=/usr/local/bin/:$PATH
 
 if [ "$INFLUXDB_START" = "true" ]
 then
@@ -19,6 +19,6 @@ then
         -u $INFLUXDB_USER \
         -p $INFLUXDB_PASSWORD \
         -b $INFLUXDB_BUCKET
-    influx auth create --user $INFLUXDB_USER --org $INFLUXDB_BUCKET --read-buckets --write-buckets
+    influx auth create --user $INFLUXDB_USER --org $INFLUXDB_ORG --read-buckets --write-buckets
 fi
 
