9d32b87a1e20a2ba19e5d1d2bec81b7c043c55ca
David Hempston <cyber_veio@hotmail.com>
2023-09-04 15:01:45 +0100

    Use full path for influx

--- tests/influxdb-tests.sh
+++ tests/influxdb-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-influx write --bucket $INFLUXDB_BUCKET "m,host=host1 field1=1.2,field2=5i 1640995200000000000"
+/usr/local/bin/influx write --bucket $INFLUXDB_BUCKET "m,host=host1 field1=1.2,field2=5i 1640995200000000000"
