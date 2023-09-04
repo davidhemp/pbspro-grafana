30b63c12d1d30d1e769e5cc1a39e95c8f070ef6b
David Hempston <cyber_veio@hotmail.com>
2023-09-04 09:07:13 +0100

    Missing bracket

--- tests/check-jobs.sh
+++ tests/check-jobs.sh
@@ -3,7 +3,7 @@
 EXIT_CODE=0
 for line in $(qstat | grep runner); do 
   job_target=$(echo "line" | awk '{ print $2}' | cut -d"-" -f1)
-  final_queue=$(echo "line" | awk '{ $6 }'
+  final_queue=$(echo "line" | awk '{ $6 }')
   if [[ "${job_target}" != "${final_qeueu}" ]]; then
     echo "Error found in: ${line}"
     EXIT_CODE=1
