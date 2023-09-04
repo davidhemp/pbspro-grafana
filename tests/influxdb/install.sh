c750face9307df98e93a40440b0819d76445784c
David Hempston <cyber_veio@hotmail.com>
2023-09-04 16:13:46 +0100

    Mising env var to start influxdb

--- tests/influxdb/install.sh
+++ tests/influxdb/install.sh
@@ -3,13 +3,12 @@ export INFLUXDB_VERSION="2.7.1"
 export INFLUXDB_DOWNLOAD_PATH="https://dl.influxdata.com/influxdb/releases"
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
 tar xvfz influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
-sudo cp influxdb2_linux_amd64/influxd /usr/local/bin/
-rm -r influxdb2_linux_amd64/
+sudo cp influxdb2_linux_amd64/influxdb /usr/local/bin/
 
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-client-$INFLUXDB_VERSION-linux-amd64.tar.gz
 tar xvfz influxdb2-client-$INFLUXDB_VERSION-linux-amd64.tar.gz
 sudo cp influx /usr/local/bin/
-rm *.tar.gz
+export PATH:/usr/local/bin/:$PATH
 
 if [ "$INFLUXDB_START" = "true" ]
 then
