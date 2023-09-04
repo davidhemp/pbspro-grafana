b41b394d2c380fff922fd0dcf7cad8430fccd400
David Hempston <cyber_veio@hotmail.com>
2023-09-04 15:12:39 +0100

    Setup influxdb

--- tests/influxdb/install-influxdb.sh
+++ tests/influxdb/install-influxdb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export INFLUXDB_VERSION="2.7.1"
 export INFLUXDB_DOWNLOAD_PATH="https://dl.influxdata.com/influxdb/releases"
 wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-$INFLUXDB_VERSION-linux-amd64.tar.gz
@@ -9,3 +10,17 @@ wget -q $INFLUXDB_DOWNLOAD_PATH/influxdb2-client-$INFLUXDB_VERSION-linux-amd64.t
 tar xvfz influxdb2-client-$INFLUXDB_VERSION-linux-amd64.tar.gz
 sudo cp influx /usr/local/bin/
 rm *.tar.gz
+
+if [ "$INFLUXDB_START" = "true" ]
+then
+    influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
+    until curl -s http://localhost:8086/health; do sleep 1; done
+    influx setup --host http://localhost:8086 -f \
+        -o $INFLUXDB_ORG \
+        -u $INFLUXDB_USER \
+        -p $INFLUXDB_PASSWORD \
+        -b $INFLUXDB_BUCKET
+    influx auth create --user $INFLUXDB_USER --org $INFLUXDB_BUCKET --read-buckets --write-buckets
+    export INFLUXDB_TOKEN=$(influx auth list | grep $INFLUXDB_USER | grep -o "[[:alnum:]\_\-]*==")
+fi
+
