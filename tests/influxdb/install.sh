79a8673819b9f46e5b840c8c81d3de849401cdf8
David Hempston <cyber_veio@hotmail.com>
2023-09-04 16:07:59 +0100

    Starting influxdb outside script

--- tests/influxdb/install.sh
+++ tests/influxdb/install.sh
@@ -13,7 +13,7 @@ rm *.tar.gz
 
 if [ "$INFLUXDB_START" = "true" ]
 then
-    influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
+    /usr/local/bin/influxd --http-bind-address :8086 --reporting-disabled > /dev/null 2>&1 &
     until curl -s http://localhost:8086/health; do sleep 1; done
     influx setup --host http://localhost:8086 -f \
         -o $INFLUXDB_ORG \
@@ -22,5 +22,6 @@ then
         -b $INFLUXDB_BUCKET
     influx auth create --user $INFLUXDB_USER --org $INFLUXDB_BUCKET --read-buckets --write-buckets
     export INFLUXDB_TOKEN=$(influx auth list | grep $INFLUXDB_USER | grep -o "[[:alnum:]\_\-]*==")
+    influx auth list
 fi
 
