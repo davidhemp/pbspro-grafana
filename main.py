ccb33fb12b2fa2dc1db331e0b4fa8f1be4c25d2d
David Hempston <cyber_veio@hotmail.com>
2025-07-18 13:15:48 +0000

    Single cluster

--- main.py
+++ main.py
@@ -13,22 +13,18 @@ with open("config.yaml", 'r') as file:
 conn = influxdb.Connector(config["influx"])
 
 queue_usage = qstat.queue_utilsation()
-for cluster in ["cx", "hx"]:
-    for queue in qmgr.routing_queues(cluster):
-        queue_usage[cluster]["running"] += queue_usage[queue]["running"]
-        queue_usage[cluster]["queued"] += queue_usage[queue]["queued"]
+for queue in qmgr.routing_queues(config["cluster"]):
+    queue_usage[config["cluster"]]["running"] += queue_usage[queue]["running"]
+    queue_usage[config["cluster"]]["queued"] += queue_usage[queue]["queued"]
     
 for queue, state in queue_usage.items():
-        p = influxdb_client.Point("jobs").tag("queue", queue).field("Running", state["running"])
-        conn.write(p)
-        p = influxdb_client.Point("jobs").tag("queue", queue).field("Queued", state["queued"])
-        conn.write(p)
+    p = influxdb_client.Point("jobs").tag("queue", queue).field("Running", state["running"])
+    conn.write(p)
+    p = influxdb_client.Point("jobs").tag("queue", queue).field("Queued", state["queued"])
+    conn.write(p)
 
-hx_max_ncpus = qmgr.cluster_capacity("hx")
-cx_max_ncpus = qmgr.cluster_capacity("cx")
+max_ncpus = qmgr.cluster_capacity(config["cluster"])
 
-p = influxdb_client.Point("jobs").tag("queue", "hx").field("max_ncpus", hx_max_ncpus)
-conn.write(p) 
-p = influxdb_client.Point("jobs").tag("queue", "cx").field("max_ncpus", cx_max_ncpus)
+p = influxdb_client.Point("jobs").tag("queue", config["cluster"]).field("max_ncpus", max_ncpus)
 conn.write(p) 
     
