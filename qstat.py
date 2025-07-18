a865117fd915ac1862df57ed0bc8ac77ddd749fc
David Hempston <cyber_veio@hotmail.com>
2025-07-18 13:53:55 +0000

    Total of queued and running

--- qstat.py
+++ qstat.py
@@ -4,9 +4,12 @@ import re
 def queue_utilsation():
     qstat_string = check_output(["qstat", "-Q", "-f", "-F", "dsv"]).decode()
     queues = {}
+    queues["Total"] = {"queued": 0, "running": 0}
     for queue in qstat_string.split("\n"):
         m = re.search("Queue: (\w+).*Queued:([0-9]*).*Running:([0-9]*)", queue)
         if m:
             queues[m.group(1)] = {"queued": int(m.group(2)), "running": int(m.group(3))}
+            queues["Total"]["queued"] += int(m.group(2))
+            queues["Total"]["running"] += int(m.group(3))
     return queues
 
